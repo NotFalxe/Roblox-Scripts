7c02f811335bc2da9e7ecd8098b0a345a3b80723
NotFalxe <96351276+NotFalxe@users.noreply.github.com>
2023-02-09 17:27:14 +0800

    Update DaHood Animation.lua

--- DaHood Animation.lua
+++ DaHood Animation.lua
@@ -8,7 +8,7 @@ Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
 Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=10921160088"
 Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=1083439238"
-Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=10921159222"
+Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=10921148939"
 game.Players.LocalPlayer.Character.Humanoid.Jump = false
 wait(1)
 end
