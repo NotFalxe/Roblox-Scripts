266f69c67ffb5d93c9b416238ec4f640560dce63
NotFalxe <96351276+NotFalxe@users.noreply.github.com>
2023-06-05 15:16:29 +0800

    Update da hood animation pack.lua

--- da hood animation pack.lua
+++ da hood animation pack.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNotZack/Da-Hood/main/Animation%20Pack.lua", true))()
-
--- credits: Zackary#1011 ilovebee#6666
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/DaHood/AnimationPack.lua"))()
